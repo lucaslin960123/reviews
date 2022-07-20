d42db15941dcef97b2347a1fb253851b486e70c8
Lucas Lin <lucaslin0123@icloud.com>
2022-07-20 13:34:34 +0800

    latest version

--- reviews.py
+++ reviews.py
@@ -18,6 +18,15 @@ with open('reviews.txt','r') as f:
 		i += 1
 print('The average of the reviews is',int(total/int(len(data))))
 
-
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('There are', len(new), 'of imformation','less than 100 words.')
 	
 
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('There are', len(good),'of reviews got good in their messages.')
