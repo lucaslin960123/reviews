bd132dd16a4dc455ac961e7315142884367f2a8b
Lucas Lin <lucaslin0123@icloud.com>
2022-07-20 11:27:50 +0800

    Final version

--- reviews.py
+++ reviews.py
@@ -6,6 +6,18 @@ with open('reviews.txt', 'r') as f:
 		i += 1
 		if i % 1000 == 0:
 			print(len(data))
-		
+print('There are',len(data),'of information')
+
+data = []
+total = 0
+i = 0
+with open('reviews.txt','r') as f:
+	for line in f:
+		data.append(line)
+		total += len(data[i])
+		i += 1
+print('The average of the reviews is',int(total/int(len(data))))
+
+
 	
 
